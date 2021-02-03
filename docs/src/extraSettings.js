2db8a61697b512a9549df94832236691d84decbb
bobbywyx <yixinbobbywu@outlook.com>
2021-02-03 21:16:40 +0800

    合成cyh

--- docs/src/extraSettings.js
+++ docs/src/extraSettings.js
@@ -9,7 +9,7 @@ let extraScore = 1;
 let wuDi = true;
 
 // 第一个水果：修改数字为 0-10, 0 为葡萄，9 为半个西瓜
-let firstFruit = 9;
+let firstFruit = 0;
 
 // 水果合成反转：false 改为 true
 let reverseLevelUp = false;
@@ -19,7 +19,7 @@ const minRandomFruitNum = reverseLevelUp ? 6 : 0; // 生成随机水果最小值
 const maxRandomFruitNum = reverseLevelUp ? 11 : 5; // 生成随机水果最大值（1-11）0 为葡萄，9 为半个西瓜
 let setFruits = {
   // 指定前几次生成的水果，可填入任意数量的数字，0 为葡萄，9 为半个西瓜
-  startFruits: reverseLevelUp ? [10, 10, 9, 8, 8, 7] : [9, 9, 9, 9, 9, 9],
+  startFruits: reverseLevelUp ? [10, 10, 9, 8, 8, 7] : [0, 1, 2, 3, 4, 5],
   randomFunction: () => {
     return minRandomFruitNum + Math.floor(Math.random() * (maxRandomFruitNum - minRandomFruitNum));
   }
@@ -32,13 +32,13 @@ let fruitQTan = false;
 let fruitSlowDown = false;
 
 // 点击右上方图标更换水果：false 改为 true 即可，暂未支持
-let clickChangeFruit = false;
+let clickChangeFruit = true;
 
 // 广告链接：false 或为空字符串表示不会跳转到广告
 let adLink = '';
 
 // 修改网页标题：将 "合成大西瓜" 进行替换
-document.getElementsByTagName("title")[0].innerText = '合成大西瓜';
+document.getElementsByTagName("title")[0].innerText = '合成cyh';
 
 // 开启选分弹窗：将 false 改为 true
 let selectModal = false;
